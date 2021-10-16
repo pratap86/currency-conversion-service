5f6bd1f15cc8f964b9c034246be2974afa5ee251
pratap86 <narayanpratap86@gmail.com>
2021-10-16 11:54:14 +0530

    modified dynamic port invocation

--- src/main/java/com/pratap/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
+++ src/main/java/com/pratap/microservices/currencyconversionservice/proxy/CurrencyExchangeProxy.java
@@ -5,10 +5,10 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "currency-exchange", url = "localhost:8000/currency-exchange")
+@FeignClient(name = "currency-exchange")
 public interface CurrencyExchangeProxy {
 
-    @GetMapping("/from/{from}/to/{to}")
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
     public CurrencyExchangeResponseModel retrieveExchangeValue
             (@PathVariable("from") String from, @PathVariable("to") String to);
 }
